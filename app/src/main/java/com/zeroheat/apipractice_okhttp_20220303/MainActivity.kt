63033ff27a4a1c36079f776600cf208c5b4959fa
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-03 16:22:31 +0900

    binding변수 이용 -> 이벤트 처리 진행

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/MainActivity.kt
@@ -2,10 +2,40 @@ package com.zeroheat.apipractice_okhttp_20220303
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.zeroheat.apipractice_okhttp_20220303.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        setupEvent()
+        setValues()
     }
+
+    fun setupEvent(){
+        binding.btnLogin.setOnClickListener {
+
+//            id / pw 추출
+
+            val inputId = binding.edtId.text.toString()
+            val inputPw = binding.edtPassword.text.toString()
+
+//            API서버에 아이디 / 비번을 보내서 실제로 회원인지 검사 => 로그인 시도
+
+
+        }
+
+    }
+
+    fun setValues(){
+
+    }
+
+
+
 }
