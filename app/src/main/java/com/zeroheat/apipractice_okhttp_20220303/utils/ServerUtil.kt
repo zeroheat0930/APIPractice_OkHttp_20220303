6bd1a66dfd06bb9dae5ad04f6aabc11dcd1bb73a
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-04 14:48:16 +0900

    인터페이스를 이용해서, 로그인 결과분석을 다른 클래스의 할 일로 넘기기.

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/utils/ServerUtil.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/utils/ServerUtil.kt
@@ -7,6 +7,23 @@ import java.io.IOException
 
 class ServerUtil {
 
+//    서버유틸로 돌아온 응답을 => 액티비티에서 처리하도록, 일처리 넘기기.
+//    나에게 생긴일을 > 다른 클래스에게 처리 요청 : interface 활용
+
+    interface JsonResponseHandler {
+        fun onResponse( jsonObj: JSONObject )
+    }
+
+
+
+
+
+
+
+
+
+
+
 //    서버에 Request를 날리는 역할.
 //    함수를 만들려고 하는데, 어떤 객체가 실행해도 결과만 잘 나오면 그만인 함수
 //    코틀린에서 static에 해당하는 개념? companion object {} 에 만들자.
@@ -18,8 +35,13 @@ class ServerUtil {
        private val BASE_URL = "http://54.180.52.26"
 
 
+//        handler : 이 함수를 쓰는 화면에서, JSON 분석을 어떻게 / UI에서 어떻게 활용할지 방안. (인터페이스)
+//         - 처리방안을 임시로 비워두려면, null 대입 허용.
+
+
+
 //        로그인 기능 호출 함수
-        fun postRequestLogin (id: String, pw: String) {
+fun postRequestLogin( id: String, pw: String, handler: JsonResponseHandler? ) {
 
 //            Request 제작 -> 실제 호출 -> 서버의 응답을, 화면에 전달
 
@@ -72,35 +94,12 @@ class ServerUtil {
 
 
                     val jsonObj = JSONObject(bodyString)
-                    Log.d("서버테스트", jsonObj.toString())
+                    Log.d("서버응답", jsonObj.toString())
 
- //                    연습 : 로그인 성공 / 실패 에 따른 로그 출력
-//                    "code" 이름표의 Int를 추출, 그 값을 if로 물어보자.
+//                    실제 : handler 변수에, jsonObj를 가지고 화면에서 어떻게 처리할지 계획이 들어와있다.
+//                    (계획이 되어있을때만) 해당 계획을 실행하자.
 
-                    val code = jsonObj.getInt("code")
-
-                    if (code == 200) {
-                        Log.d("로그인시도", "성공!")
-
-                        val dataObj = jsonObj.getJSONObject("data")
-                        val userObj = dataObj.getJSONObject("user")
-
-                        val nickname = userObj.getString("nick_name")
-
-                        Log.d("로그인한사람이 닉네임", nickname)
-
-                    }
-                    else {
-                        Log.d("로그인시도", "실패.")
-
-                        val message = jsonObj.getString("message")
-
-                        Log.d("실패 사유", message)
-
-
-
-
-                    }
+                    handler?.onResponse(jsonObj)
 
 
 
