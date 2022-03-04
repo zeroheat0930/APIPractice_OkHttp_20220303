a904f06edb99de012432760a316ebd22495a4219
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-04 15:03:47 +0900

    코틀린단에서도 바인딩 변수 설정

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/SignUpActivity.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/SignUpActivity.kt
@@ -2,10 +2,16 @@ package com.zeroheat.apipractice_okhttp_20220303
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.zeroheat.apipractice_okhttp_20220303.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+
+    lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+      binding =  DataBindingUtil.setContentView(this,R.layout.activity_sign_up)
     }
 }
