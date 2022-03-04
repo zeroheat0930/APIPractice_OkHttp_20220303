76b61a322b56bccbe0afa57e234d010ce7ebec43
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-04 10:10:28 +0900

    파라미터 세팅  - 어느 공간에 : FormData  - 어떤 데이터들을 : email/password

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/utils/ServerUtil.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/utils/ServerUtil.kt
@@ -1,5 +1,7 @@
 package com.zeroheat.apipractice_okhttp_20220303.utils
 
+import okhttp3.FormBody
+
 class ServerUtil {
 
 //    서버에 Request를 날리는 역할.
@@ -21,6 +23,12 @@ class ServerUtil {
 //            제작 1) 어느 주소(url)로 접근할지? => 서버 주소 + 기능 주소
             val urlString = "${BASE_URL}/user"
 
+//            제작 2) 파라미터 담아주기 => 어떤 이름표 / 어느 공간에
+
+            val formData = FormBody.Builder()
+                .add("email", id)
+                .add("password", pw)
+                .build()
         }
     }
 
