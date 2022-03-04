e284d87908801ec335f211291256be6f7a4a173f
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-04 14:51:33 +0900

    서버통신이 완료된 후의 처리를, 화면단에서 코딩  - 토스트를 띄우려니 앱이 죽는다.

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/MainActivity.kt
@@ -2,9 +2,11 @@ package com.zeroheat.apipractice_okhttp_20220303
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.zeroheat.apipractice_okhttp_20220303.databinding.ActivityMainBinding
 import com.zeroheat.apipractice_okhttp_20220303.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,7 +30,18 @@ class MainActivity : AppCompatActivity() {
 
 //            API서버에 아이디 / 비번을 보내서 실제로 회원인지 검사 => 로그인 시도
 
-            ServerUtil.postRequestLogin(inputId, inputPw)
+            ServerUtil.postRequestLogin(inputId, inputPw, object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+//                    화면의 입장에서, 로그인 결과를 받아서 처리할 코드.
+                val code = jsonObj.getInt("code")
+                    if(code == 200) {
+
+                    } else{
+                        Toast.makeText(this@MainActivity,"로그인 실패",Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            })
 
         }
 
