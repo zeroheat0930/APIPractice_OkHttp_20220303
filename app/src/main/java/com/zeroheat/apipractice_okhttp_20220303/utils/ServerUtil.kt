e24a874ba82e2df9d62c303849084c8ab6e14dc1
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-04 10:35:37 +0900

    실제 리퀘스트 호출 진행

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/utils/ServerUtil.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/utils/ServerUtil.kt
@@ -1,7 +1,7 @@
 package com.zeroheat.apipractice_okhttp_20220303.utils
 
-import okhttp3.FormBody
-import okhttp3.Request
+import okhttp3.*
+import java.io.IOException
 
 class ServerUtil {
 
@@ -36,6 +36,29 @@ class ServerUtil {
                 .url(urlString)
                 .post(formData)
                 .build()
+
+//            종합한 Request도 실제 호출은 해 줘야 API 호출이 실행됨. (startActivity 같은 동작 필요함)
+//            실제 호출 : 앱이 클라이언트로써 동작 > OkHttpClient 클래스
+
+            val client = OkHttpClient()
+
+
+//            OkHttpClinet 객체를 이용 > 서버에 로그인 기능 실제 호출
+//            호출을 했으면, 서버가 수행한 결과를 받아서 처리.
+//            => 서버에 다녀와서 할 일을 등록 : enqueue( Callback)
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+                }
+
+            })
+
+
 
         }
     }
