7967b131c2a08f794ad8cc2867dace1533340e35
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-04 16:47:50 +0900

    회원가입의 응답이 돌아오면, JSONObject로 응답을 가공해서 => 화면단에 JSON 분석 코드 실행

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/utils/ServerUtil.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/utils/ServerUtil.kt
@@ -125,6 +125,22 @@ class ServerUtil {
                 .url(urlString)
                 .put(formData)
                 .build()
+
+            val client = OkHttpClient()
+            client.newCall(request).enqueue(object : Callback{
+                override fun onFailure(call: Call, e: IOException) {
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+                    val bodyString = response.body!!.string()
+                    val jsonObj = JSONObject(bodyString)
+                    Log.d("서버응답", jsonObj.toString())
+                    handler?.onResponse(jsonObj)
+
+                }
+            })
         }
 
     }
