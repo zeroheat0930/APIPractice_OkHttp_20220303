8279acf81e5ac441f49c0929b4d61838372b4946
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-04 10:18:01 +0900

    Request 정보 종합 - 어느 주소 : url - 어떤 메쏘드 : post - 어떤 데이터 (파라미터) : formData

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/utils/ServerUtil.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/utils/ServerUtil.kt
@@ -1,6 +1,7 @@
 package com.zeroheat.apipractice_okhttp_20220303.utils
 
 import okhttp3.FormBody
+import okhttp3.Request
 
 class ServerUtil {
 
@@ -29,6 +30,13 @@ class ServerUtil {
                 .add("email", id)
                 .add("password", pw)
                 .build()
+
+//            제작 3) 모든 Request 정보를 종합한 객체 생성. (어느 주소로 + 어느 메쏘드로 + 어떤 파라미터를)
+            val request = Request.Builder()
+                .url(urlString)
+                .post(formData)
+                .build()
+
         }
     }
 
