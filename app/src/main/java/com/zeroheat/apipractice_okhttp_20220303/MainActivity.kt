876dacdc44e7d064bd68a1a3976bce3d0459f7e2
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-07 16:58:34 +0900

    topics라는 JSONArray 추출

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.zeroheat.apipractice_okhttp_20220303.databinding.ActivityMainBinding
+import com.zeroheat.apipractice_okhttp_20220303.datas.TopicData
 import com.zeroheat.apipractice_okhttp_20220303.utils.ServerUtil
 import org.json.JSONObject
 
@@ -11,10 +12,15 @@ class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+//    실제로 서버가 내려주는 주제 목록을 담을 그릇정보
+    val mTopicList = ArrayList<TopicData>()
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-    setupEvents()
+        setupEvents()
         setValues()
 
     }
@@ -23,38 +29,33 @@ class MainActivity : BaseActivity() {
     override fun setupEvents() {
 
 
-
-
     }
 
     override fun setValues() {
 
-//        화면의 텍스트뷰에 닉네임을 보여주기 위한 작업
-        ServerUtil.getRequestMyInfo(mContext,  object : ServerUtil.JsonResponseHandler {
+//        메인 화면 정보 가져오기 => API 호출 / 응답 처리
+        getTopicListFromServer()
+
+    }
+
+    fun getTopicListFromServer() {
+
+        ServerUtil.getRequestMainInfo(mContext, object : ServerUtil.JsonResponseHandler {
+
             override fun onResponse(jsonObj: JSONObject) {
+
+//                서버가 주는 토론 주제 목록 파싱 => mTopicList에 추가하기.
 
                 val dataObj = jsonObj.getJSONObject("data")
 
-                val userObj = dataObj.getJSONObject("user")
+                val topicsArr = dataObj.getJSONArray("topics")
 
-                val nickname = userObj.getString("nick_name")
-
-                runOnUiThread {
-
-                    binding.txtLoginUserNickname.text = nickname
-
-                }
+//                topicsArr 내부를 하나씩 추출 (JSONObject {  }) => TopicData() 로 변환.
 
             }
 
         })
 
     }
-
-
-
-
-
-
 
 }
