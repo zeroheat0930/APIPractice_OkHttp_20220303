35d06dde3c4ce309b3da81ab85196ba97c5cd208
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-07 16:47:09 +0900

    리스트뷰 어댑터 - getView 함수 작성

---
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/adapters/TopicAdapter.kt
@@ -0,0 +1,36 @@
+package com.zeroheat.apipractice_okhttp_20220303.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.zeroheat.apipractice_okhttp_20220303.R
+import com.zeroheat.apipractice_okhttp_20220303.datas.TopicData
+
+class TopicAdapter  (
+    val mContext: Context,
+    resId: Int,
+    val mList: List<TopicData>
+) : ArrayAdapter<TopicData>(mContext, resId, mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow =  LayoutInflater.from(mContext).inflate(R.layout.topic_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+
+    }
+
+
+
+
+
+
+
+}
