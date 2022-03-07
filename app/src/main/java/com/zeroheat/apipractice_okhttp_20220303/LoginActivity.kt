2db655d652b8da915ec0a340ef17ccc421eba1dc
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-07 09:41:46 +0900

    메인액티비티 이름 변경 > 로그인 액티비티

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/LoginActivity.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/LoginActivity.kt
@@ -1,22 +1,21 @@
 package com.zeroheat.apipractice_okhttp_20220303
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import com.zeroheat.apipractice_okhttp_20220303.databinding.ActivityMainBinding
+import com.zeroheat.apipractice_okhttp_20220303.databinding.ActivityLoginBinding
 import com.zeroheat.apipractice_okhttp_20220303.utils.ServerUtil
 import org.json.JSONObject
 
-class MainActivity : BaseActivity() {
+class LoginActivity : BaseActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityLoginBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
         setupEvents()
         setValues()
     }
