d4119fe58700e2ac4b877b9c1c26114cbac57cd0
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-07 11:17:06 +0900

    닉네임 중복검사 기능 구현  - 이미 만들어져 있는 함수 재활용

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/SignUpActivity.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/SignUpActivity.kt
@@ -24,6 +24,31 @@ class SignUpActivity : BaseActivity() {
     override fun setupEvents() {
 
         //        닉네임 검사 버튼 기능.
+        binding.btnNicknameCheck.setOnClickListener {
+
+            val inputNickname = binding.edtNickname.text.toString()
+
+            ServerUtil.getRequestDuplicatedCheck("NICK_NAME", inputNickname, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(jsonObj: JSONObject) {
+
+                    val code = jsonObj.getInt("code")
+
+                    runOnUiThread {
+
+                        if (code == 200) {
+                            binding.txtNicknameCheckResult.text = "사용해도 좋습니다."
+                        }
+                        else {
+                            binding.txtNicknameCheckResult.text = "다른 닉네임으로 사용해주세요."
+                        }
+
+                    }
+
+                }
+
+            })
+
+        }
 
 
 
