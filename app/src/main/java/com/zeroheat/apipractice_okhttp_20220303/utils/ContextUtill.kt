c4e028cc9ce27293c6cb73cbe9b5d3bc8f91bb14
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-07 14:16:01 +0900

    토큰 항목 변수 생성 / setter 함수 제작

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/utils/ContextUtill.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/utils/ContextUtill.kt
@@ -1,5 +1,7 @@
 package com.zeroheat.apipractice_okhttp_20220303.utils
 
+import android.content.Context
+
 class ContextUtill {
 
     companion object {
@@ -7,6 +9,24 @@ class ContextUtill {
 //        메모장 파일 이름처럼,  SharedPreferences의 이름 설정.
 
         private val prefName = "OkHttpPracticePref"
+
+//        저장할 데이터의 항목명도 변수로 만들어두자.
+
+        val TOKEN = "TOKEN"
+
+//        데이터 저장 함수 (setter) / 조회 함수 (getter) 별개로 작성.
+//        TOKEN 항목에 저장 => token 항목 조회? 데이터 인식 X. 대소문자까지 동일해야함.
+//        오타를 줄이고, 코딩 편하게 하는 조치.
+
+        fun setToken(context: Context, token: String ) {
+
+//            메모장 파일을 열자.
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+//            입력 들어온 token 내용 (TOKEN 항목에) 저장.
+            pref.edit().putString(TOKEN, token).apply()
+
+        }
+
 
     }
 
