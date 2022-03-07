32a331ca95926d610d0e423da836f645274f17ec
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-07 15:58:27 +0900

    메인화면 정보 로드 API 기능만 추가

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/utils/ServerUtil.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/utils/ServerUtil.kt
@@ -215,7 +215,37 @@ class ServerUtil {
 
         }
 
+        fun getRequestMainInfo( context: Context, handler: JsonResponseHandler? ){
+            val urlBuilder = "${BASE_URL}/v2/main_info".toHttpUrlOrNull()!!.newBuilder()
+                .build() // 쿼리파라미터를 담을게 없다. 바로 build로 마무리.
 
+            val urlString = urlBuilder.toString()
+
+            val request = Request.Builder()
+                .url(urlString)
+                .get()
+                .header( "X-Http-Token", ContextUtill.getToken(context) )  // ContextUtil을 통해, 저장된 토큰을 받아서 첨부.
+                .build()
+
+            val client = OkHttpClient()
+
+            client.newCall(request).enqueue(object  :  Callback {
+                override fun onFailure(call: Call, e: IOException) {
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+                    val jsonObj = JSONObject(  response.body!!.string()  )
+                    Log.d("서버응답", jsonObj.toString())
+                    handler?.onResponse(jsonObj)
+
+                }
+
+            })
+
+
+        }
 
 
 
