6a9427f19501eed194819c10161f6dde01df1cef
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-07 10:46:27 +0900

    이메일 중복 체크 결과 UI 반영

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/SignUpActivity.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/SignUpActivity.kt
@@ -31,7 +31,30 @@ class SignUpActivity : BaseActivity() {
 //            서버의 중복확인 기능 (/user_check - GET) API 활용 => ServerUtil에 함수 추가, 가져다 활용
 //            그 응답 code값에 따라 다른 문구 배치.
 
-            ServerUtil.getRequestDuplicatedCheck("Email", inputEmail, null)
+            ServerUtil.getRequestDuplicatedCheck("EMAIL", inputEmail, object : ServerUtil.JsonResponseHandler {
+
+                override fun onResponse(jsonObj: JSONObject) {
+
+//                    code 값에 따라 이메일 사용 가능 여부.
+
+                    val code = jsonObj.getInt("code")
+
+                    runOnUiThread {
+
+                        when(code) {
+                            200 -> {
+                                binding.txtEmailCheckResult.text = "사용해도 좋은 이메일입니다."
+                            }
+                            else -> {
+                                binding.txtEmailCheckResult.text = "다른 이메일로 다시 검사해주세요."
+                            }
+                        }
+
+                    }
+
+                }
+
+            } )
 
         }
 
