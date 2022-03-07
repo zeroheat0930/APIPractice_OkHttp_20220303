315d4816e620473476aba3168744c78dd2135d31
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-07 16:38:17 +0900

    토론 주제 표현용 데이터 클래스 추가  - 생성자 X, 멤버변수만 추가  - 서버의 데이터를 참고해서 추가

---
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/datas/TopicData.kt
@@ -0,0 +1,10 @@
+package com.zeroheat.apipractice_okhttp_20220303.datas
+
+class TopicData {
+
+    var id = 0 // id는 Int라고 명시.
+    var title = "" // title은 String이라고 명시.
+    var imageURL = "" // 서버 : img_url, 앱 : imageURL 변수명 다른 경우.
+    var replyCount = 0
+
+}
