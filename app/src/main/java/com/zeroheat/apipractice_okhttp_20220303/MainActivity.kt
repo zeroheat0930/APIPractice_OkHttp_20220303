0992b4c70eae8e6ea25bdd53e561b76b10e3f7e3
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-07 15:48:33 +0900

    연습문제 마무리  - 토큰을 첨부해서, 토큰에 맞는 내 정보를 받아오기

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.zeroheat.apipractice_okhttp_20220303.databinding.ActivityMainBinding
 import com.zeroheat.apipractice_okhttp_20220303.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -29,7 +30,24 @@ class MainActivity : BaseActivity() {
     override fun setValues() {
 
 //        화면의 텍스트뷰에 닉네임을 보여주기 위한 작업
-        ServerUtil.
+        ServerUtil.getRequestMyInfo(mContext,  object : ServerUtil.JsonResponseHandler {
+            override fun onResponse(jsonObj: JSONObject) {
+
+                val dataObj = jsonObj.getJSONObject("data")
+
+                val userObj = dataObj.getJSONObject("user")
+
+                val nickname = userObj.getString("nick_name")
+
+                runOnUiThread {
+
+                    binding.txtLoginUserNickname.text = nickname
+
+                }
+
+            }
+
+        })
 
     }
 
