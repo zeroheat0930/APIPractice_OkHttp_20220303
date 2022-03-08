eb61485f5d550670b49a47d3e20020a184d1914b
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-08 12:16:55 +0900

    초기 진입 / 상세 조회 API 모두, 같은 코드를 실행하도록 UI 세팅 코드 함수화

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/VIewTopicDetailActivity.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/VIewTopicDetailActivity.kt
@@ -28,10 +28,17 @@ class VIewTopicDetailActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        setTopicDataToUi()
+        getTopicDetailFromServer()
+    }
+
+    fun setTopicDataToUi(){
+
+//        토론 주제에 대한 데이터들을, UI에 반영하는 함수.
+//        화면 초기 진입 실행 + 서버에서 다시 받아왔을때도 실행.
         binding.txtTitle.text = mTopicData.title
         Glide.with(mContext).load(mTopicData.imageURL).into(binding.imgTopicBackground)
-
-        getTopicDetailFromServer()
     }
 
     fun getTopicDetailFromServer() {
@@ -47,6 +54,10 @@ class VIewTopicDetailActivity : BaseActivity() {
 
 //                변환된 객체를, mTopicData로 다시 대입. => UI 반영도 다시 실행.
                 mTopicData = topicData
+
+                runOnUiThread {
+                    setTopicDataToUi()
+                }
 
             }
 
