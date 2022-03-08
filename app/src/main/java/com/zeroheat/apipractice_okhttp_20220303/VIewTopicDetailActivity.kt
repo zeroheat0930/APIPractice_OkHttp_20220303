b1f58d7516945873fbee4caaa0763c1bc447f783
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-08 14:51:48 +0900

    투표 버튼 추가, API 연결 함수 추가

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/VIewTopicDetailActivity.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/VIewTopicDetailActivity.kt
@@ -25,6 +25,19 @@ class VIewTopicDetailActivity : BaseActivity() {
     }
     override fun setupEvents() {
 
+//        btnVote1 클릭 => 첫 진영의 id값을 찾아서, 거기에 투표.
+//        서버에 전달 => API 활용.
+
+        binding.btnVote1.setOnClickListener {
+
+//            서버의 투표 API 호출
+
+//            투표 현황 새로고침 (응답)
+
+        }
+
+
+
     }
 
     override fun setValues() {
--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/utils/ServerUtil.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/utils/ServerUtil.kt
@@ -280,6 +280,41 @@ class ServerUtil {
 
         }
 
+        fun postRequestVote( context: Context, sideId: Int, handler: JsonResponseHandler? ) {
+
+            val urlString = "${BASE_URL}/topic_vote"
+
+            val formData = FormBody.Builder()
+                .add("side_id", sideId.toString())
+                .build()
+
+            val request = Request.Builder()
+                .url(urlString)
+                .post(formData)
+                .header("X-Http-Token", ContextUtill.getToken(context))
+                .build()
+
+            val client = OkHttpClient()
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+                    val bodyString = response.body!!.string()
+                    val jsonObj = JSONObject(bodyString)
+                    Log.d("서버응답", jsonObj.toString())
+                    handler?.onResponse(jsonObj)
+
+                }
+
+            })
+
+
+
+        }
     }
 
 
