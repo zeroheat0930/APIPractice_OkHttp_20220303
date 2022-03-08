59d9efa0823c41ae95f6a31764675ef5ff156685
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-08 17:01:36 +0900

    만들어져있는 SideData를 활용해서, 댓글의 지지 진영도 파싱

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/datas/ReplyData.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/datas/ReplyData.kt
@@ -9,6 +9,8 @@ class ReplyData(
 
     var writer = UserData()  // 모든 댓글에는 작성자가 있다. null 가능성이 없다.
 
+    var selectedSide = SideData() // 모든 댓글에는 선택한 진영이 있다. null 가능성 X.
+
 
     //    보조 생성자 추가 연습 : 파라미터 x.
     constructor() :  this( 0, "내용없음" )
@@ -23,7 +25,9 @@ class ReplyData(
 
             replyData.id = jsonObj.getInt("id")
             replyData.content = jsonObj.getString("content")
-            replyData.writer =  UserData.getUserDataFromJson(  jsonObj.getJSONObject("user")  )
+            replyData.writer =  UserData.getUserDataFromJson(  jsonObj.getJSONObject("user"))
+
+            replyData.selectedSide = SideData.getSideDataFromJson( jsonObj.getJSONObject("selected_side") )
 
 
             return replyData
