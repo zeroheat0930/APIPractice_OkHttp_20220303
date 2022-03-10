8bf2a694c551072bb6950f0a742484faba0ffa26
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-10 15:26:52 +0900

    실제 토론 주제 데이터를 넘겨서, 제목 / 진영의 제목 표시

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/EditReplyActivity.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/EditReplyActivity.kt
@@ -4,24 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.zeroheat.apipractice_okhttp_20220303.databinding.ActivityEditReplyBinding
+import com.zeroheat.apipractice_okhttp_20220303.datas.TopicData
 
 class EditReplyActivity : BaseActivity() {
 
     lateinit var binding: ActivityEditReplyBinding
 
+    //    어느 토론 주제에서 온건지. + 무조건 선택한 진영도 있다!
+    lateinit var mTopicData : TopicData
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_edit_reply)
+        mTopicData = intent.getSerializableExtra("topic") as TopicData
         setupEvents()
         setValues()
     }
 
     override fun setupEvents() {
-        TODO("Not yet implemented")
+
     }
 
     override fun setValues() {
-        TODO("Not yet implemented")
+
+        binding.txtTopicTitle.text =  mTopicData.title
+        binding.txtSideTitle.text = mTopicData.mySelectedSide!!.title
+
     }
 
 }
