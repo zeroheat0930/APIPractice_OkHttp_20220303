ee6dcdf74ce90e5ff4d3a2a6951ebc8a9d98deb5
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-10 15:02:15 +0900

    시차 보정 기능 추가

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/datas/ReplyData.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/datas/ReplyData.kt
@@ -36,6 +36,20 @@ class ReplyData(
 //        작성일시의 일시값을 그대로 복사  (원래값 : 현재 일시)
         localCal.time =  this.createdAt.time
 
+//        localCal의 값을, 내 폰 설정에 맞게 시간을 더하자.
+//        ex. 한국 : +9를 더해야함.
+//        ex. LA : -8을 더해야함.
+
+//        Calendar 객체의 기능으로, 시간대 정보 추출.
+        val localTimeZone = localCal.timeZone
+
+//        시간대가, GMT와의 시차가 얼마나 나는지? (rawOffset - 몇 ms 차이까지)
+        val diffHour = localTimeZone.rawOffset / 60 / 60 / 1000   // ms => 시간으로 변경
+
+//        구해진 시차를, 기존 시간에서 시간값으로 더해주기
+        localCal.add(Calendar.HOUR, diffHour)
+
+
         return  sdf.format(localCal.time)
     }
 
