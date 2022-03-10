2f14b5f475939831a5289d183bbb2f8de04f9f7b
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-10 14:54:21 +0900

    임시연습 - 작성일시의 항목들을 하나씩 꺼내서 일자 형태로 가공.  - 월값은 0~11이므로, 1 작게 나옴. (보정 필요)

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/adapters/ReplyAdapter.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/adapters/ReplyAdapter.kt
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter
 import android.widget.TextView
 import com.zeroheat.apipractice_okhttp_20220303.R
 import com.zeroheat.apipractice_okhttp_20220303.datas.ReplyData
+import java.util.*
 
 class ReplyAdapter(
     val mContext: Context,
@@ -29,11 +30,17 @@ class ReplyAdapter(
         val txtSelectedSide = row.findViewById<TextView>(R.id.txtSelectedSide)
         val txtWriterNickname = row.findViewById<TextView>(R.id.txtWriterNickname)
         val txtReplyContent = row.findViewById<TextView>(R.id.txtReplyContent)
+        val txtCreatedAt = row.findViewById<TextView>(R.id.txtCreatedAt)
 
 
         txtReplyContent.text = data.content
         txtWriterNickname.text = data.writer.nickname
         txtSelectedSide.text = "[${data.selectedSide.title}]"
+
+
+//        임시 - 작성일자만 "2022-03-10" 형태로 표현. => 연 / 월 / 일 데이터로 가공
+//        월은 1 작게 나옴. +1로 보정.
+        txtCreatedAt.text = "${ data.createdAt.get(Calendar.YEAR) }-${ data.createdAt.get(Calendar.MONTH) + 1 }-${ data.createdAt.get(Calendar.DAY_OF_MONTH) }"
 
 
         return row
