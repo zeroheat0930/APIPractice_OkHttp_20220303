406bea74fd1f79ba4699c9f7163602da76adc85f
zeroheat0930 <80299089+zeroheat0930@users.noreply.github.com>
2022-03-11 09:42:33 +0900

    게시글 작성시, 입력값 검증

--- app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/EditReplyActivity.kt
+++ app/src/main/java/com/zeroheat/apipractice_okhttp_20220303/EditReplyActivity.kt
@@ -2,6 +2,7 @@ package com.zeroheat.apipractice_okhttp_20220303
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.zeroheat.apipractice_okhttp_20220303.databinding.ActivityEditReplyBinding
 import com.zeroheat.apipractice_okhttp_20220303.datas.TopicData
@@ -23,6 +24,22 @@ class EditReplyActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        binding.btnPostReply.setOnClickListener {
+
+//            앱에서 검사 > 입력문구가 5자 미만이면 토스트, 함수 강제종료
+
+            val inputContent = binding.edtReplyContent.text.toString()
+
+            if (inputContent.length < 5){
+                Toast.makeText(mContext, "5자 이상으로 입력해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+//            입력한 내용을 > 서버 API 호출
+
+        }
+
 
     }
 
